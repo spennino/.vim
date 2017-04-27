58488751772ddec0da64a014735e83dd2d958619
Sean Pennino <pennino.sean@gmail.com>
2017-04-27 16:38:02 -0700

    Silver searcher

--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+CYAN='\033[1;36m'
+PURPLE='\033[1;35m'
+WHITE='\033[0;37m'
+YELLOW='\033[1;33m'
+OS=$(uname)
 
-echo -e "Installing .vimrc..."
+echo -e "${PURPLE}Installing .vimrc...${YELLOW}"
 ln -s ~/.vim/.vimrc ~/.vimrc
 
 if [ "$?" -ne "0" ]; then
-  read -p "~/.vimrc already exists. Would you like to overwrite it? [y/n] " prompt
+  read -p "${PURPLE}~/.vimrc already exists. Would you like to overwrite it? [y/n] " prompt
   if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
   then
     rm ~/.vimrc;
@@ -14,3 +19,24 @@ if [ "$?" -ne "0" ]; then
     exit;
   fi
 fi
+
+echo -e "${PURPLE}Installing submodules...${WHITE}"
+cd ~/.vim
+git submodule update --init --recursive > /dev/null
+
+echo -e "${PURPLE}Building YCM...${WHITE}"
+echo -e "${PURPLE}Installing cmake with brew...${WHITE}"
+brew install cmake
+
+cd ~
+mkdir .ycm_build
+cd .ycm_build
+echo -e "${PURPLE}Generating config files...${WHITE}"
+cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
+echo -e "${PURPLE}Compiling libraries...${WHITE}"
+cmake --build . --target ycm_support_libs
+
+echo -e "${PURPLE}Installing silver searcher with brew...${WHITE}"
+brew install the_silver_searcher
+
+echo -e "${CYAN}Success!${WHITE}"
