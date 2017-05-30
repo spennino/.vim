09a5864d248a2c2e820449ad87cc8c0edcd83296
Sean Pennino <pennino.sean@gmail.com>
2017-05-30 15:15:52 -0700

    Removed YCM and added Supertab

--- setup.sh
+++ setup.sh
@@ -20,23 +20,11 @@ if [ "$?" -ne "0" ]; then
   fi
 fi
 
+echo -e "${PURPLE}Installing silver searcher with brew...${WHITE}"
+brew install the_silver_searcher
+
 echo -e "${PURPLE}Installing submodules...${WHITE}"
 cd ~/.vim
 git submodule update --init --recursive > /dev/null
-
-echo -e "${PURPLE}Building YCM...${WHITE}"
-echo -e "${PURPLE}Installing cmake with brew...${WHITE}"
-brew install cmake
-
-cd ~
-mkdir .ycm_build
-cd .ycm_build
-echo -e "${PURPLE}Generating config files...${WHITE}"
-cmake -G "Unix Makefiles" . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
-echo -e "${PURPLE}Compiling libraries...${WHITE}"
-cmake --build . --target ycm_support_libs
-
-echo -e "${PURPLE}Installing silver searcher with brew...${WHITE}"
-brew install the_silver_searcher
 
 echo -e "${CYAN}Success!${WHITE}"
